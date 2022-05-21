b1685af443abc5ec8ca414a431f2426ee2bff07a
Philip Theobald <fylzero@gmail.com>
2022-05-21 01:04:09 -0500

    Code style

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,18 @@
-window._ = require('lodash');
-window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window._ = require('lodash')
+window.axios = require('axios')
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress';
-import VTooltip from 'v-tooltip';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
+import VTooltip from 'v-tooltip'
 
 // Mosha Vue Toastify
 // https://szboynono.github.io/mosha-vue-toastify/
-import moshaToast from 'mosha-vue-toastify';
-import 'mosha-vue-toastify/dist/style.css';
+import moshaToast from 'mosha-vue-toastify'
+import 'mosha-vue-toastify/dist/style.css'
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel'
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -23,8 +23,8 @@ createInertiaApp({
             .use(VTooltip)
             .use(moshaToast)
             .mixin({ methods: { route } })
-            .mount(el);
+            .mount(el)
     },
-});
+})
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({ color: '#4B5563' })
