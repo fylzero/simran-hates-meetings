09ca983c1c4fcaa8f4ce91da4f0ed8d32219806c
Shift <shift@laravelshift.com>
2022-06-30 15:25:10 +0000

    Use ES modules

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
-window._ = require('lodash')
-window.axios = require('axios')
+import _ from 'lodash'
+window._ = _
+import axios from 'axios'
+window.axios = axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 import { createApp, h } from 'vue'
