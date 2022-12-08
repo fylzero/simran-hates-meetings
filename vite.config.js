cddb6b0cb567f34c3bf7306841b30a5efa66bf0d
Philip Theobald <fylzero@gmail.com>
2022-12-07 23:45:45 -0600

    Upgraded Vite to v3 - added HMR config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import fs from 'fs'
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
-import { homedir } from 'os'
-import { resolve } from 'path'
 
-let host = 'simran-hates-meetings.test'
+const domain = 'simran-hates-meetings.test'
+const homedir = require('os').homedir()
 
 export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
+            refresh: true,
         }),
         vue({
             template: {
@@ -20,39 +19,15 @@ export default defineConfig({
                 },
             },
         }),
-        {
-            name: 'blade',
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith('.blade.php')) {
-                    server.ws.send({
-                        type: 'full-reload',
-                        path: '*',
-                    })
-                }
-            },
-        },
     ],
-    server: detectServerConfig(host),
-})
-
-function detectServerConfig(host) {
-    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
-    let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
-
-    if (!fs.existsSync(keyPath)) {
-        return {}
-    }
-
-    if (!fs.existsSync(certificatePath)) {
-        return {}
-    }
-
-    return {
-        hmr: { host },
-        host,
+    server: {
         https: {
-            key: fs.readFileSync(keyPath),
-            cert: fs.readFileSync(certificatePath),
+            key: `${homedir}/.config/valet/Certificates/${domain}.key`,
+            cert: `${homedir}/.config/valet/Certificates/${domain}.crt`,
         },
-    }
-}
+        host: domain,
+        hmr: {
+            host: domain,
+        },
+    },
+})
