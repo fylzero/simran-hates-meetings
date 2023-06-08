85cae8e073a49941add8a0e89eeb9c3c1fdeeaab
Philip Theobald <fylzero@gmail.com>
2023-06-08 00:05:08 -0500

    Centered email image

--- resources/views/mail/missed-meeting.blade.php
+++ resources/views/mail/missed-meeting.blade.php
@@ -1,9 +1,12 @@
 <x-mail::message>
+
 # Hi Simran, it's {{ auth()->check() ? auth()->user()->name : 'me' }}.
 
 ## Looks like you bailed on, yet another, meeting!  Here is your punishment quote from Quan...
 
-<img src="{{ url('/img/quan.png') }}" style="max-width: 300px; margin-top: 10px; margin-bottom: 20px;">
+<div style="text-align: center;">
+<img src="{{ url('/img/quan.png') }}" style="max-width: 300px; margin-top: 10px; margin-bottom: 20px; margin-left: auto; margin-right: auto;">
+</div>
 
 # "{{ $quote }}"
 
